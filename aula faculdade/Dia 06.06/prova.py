1e29e275a091ae08421bb6cbf86aecf2edab6e02
juannl12 <jlemos329@gmail.com>
2025-06-13 18:34:16 -0300

    prova

---
+++ aula faculdade/Dia 06.06/prova.py
@@ -0,0 +1,29 @@
+def sinal_de_transito():
+
+    cor = input("Digite a cor do sinal (verde, amarelo ou vermelho): ").strip().lower()
+
+
+
+    if cor == "verde":
+
+        print("🟢 Verde = Passe")
+
+    elif cor == "amarelo":
+
+        print("🟡 Amarelo = Atenção, prepare-se para parar")
+
+    elif cor == "vermelho":
+
+        print("🔴 Vermelho = Pare")
+
+    else:
+
+        print(" Cor inválida. Use 'verde', 'amarelo' ou 'vermelho'.")
+
+
+
+sinal_de_transito()
+
+
+
+# Gabriel Silva de Oliveira, João Augusto Dutra Baptista da Conceição, Luiz Felipe da Costa Freitas, Yuri Spinelli.
