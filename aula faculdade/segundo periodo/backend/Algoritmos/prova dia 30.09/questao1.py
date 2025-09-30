2f6a0e12c6768a6b212f0b091d98bcc9983b615e
Juan Lemos <jlemos329@gmail.com>
2025-09-30 19:47:19 -0300

    prova

---
+++ aula faculdade/segundo periodo/backend/Algoritmos/prova dia 30.09/questao1.py
@@ -0,0 +1,20 @@
+#Juan Dos Anjos lemos
+#202515173
+
+numero1 = int(input("Digite um número: "))
+numero_para_divisao = int(input("Digite um divisor: "))
+
+multiplo = numero1 % numero_para_divisao
+
+if multiplo == 0:
+    print(f"É multiplo de {numero_para_divisao}")
+else:
+    print(f"Não é multiplo de {numero_para_divisao}")
+
+
+if numero1 > 0:
+    print(f"O número {numero1} é positivo")
+elif numero1 < 0:
+    print(f"O número {numero1} é negativo")
+else:
+    print(f"O número é 0")
---
+++ aula faculdade/segundo periodo/backend/Algoritmos/prova dia 30.09/questao2.py
@@ -0,0 +1,33 @@
+#Juan Dos Anjos lemos
+#202515173
+
+print("Calculo de área\n")
+
+opcoes = str(input("Você quer calcular qual ?\nQuadrado\nTriângulo\nCírculo\n\n > > > ")).strip().lower()
+
+
+
+
+if opcoes == 'quadrado':
+    lado = float(input("Digite o lado do quadrado: "))
+    area_do_quadrado = lado * lado
+    print(f"A área do quadrado é: {area_do_quadrado}")
+
+elif opcoes == 'triangulo' or opcoes == 'triângulo':
+    base = float(input("Digite a base do triângulo: "))
+    altura = float(input("Digite a altura do triângulo: "))
+
+    area_do_triangulo = (base * altura) / 2
+    print(f"A área do Triângulo é {area_do_triangulo}")
+
+elif opcoes == 'circulo' or opcoes == 'círculo':
+    raio = float(input("Digite o raio do círculo: "))
+
+    area_do_circulo = 3.14 * (raio ** 2)
+    print(f"A area do círculo é: {area_do_circulo}")
+
+else:
+    print("Entrada Inválida.\n\nTente Novamente...")
+    
+
+
