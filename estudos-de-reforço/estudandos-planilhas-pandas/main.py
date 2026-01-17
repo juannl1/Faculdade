5dcc1d4b73e6cd5171e1e9c7a816f37affdfb448
Juan Lemos <jlemos329@gmail.com>
2026-01-17 19:39:17 -0300

    Update main.py

---
+++ estudos-de-reforço/estudandos-planilhas-pandas/main.py
@@ -0,0 +1,21 @@
+"""
+Anotações:
+
+Dicionário --> DataFrame (Tabela) --> Arquivo Final 
+"""
+
+
+import pandas as pd
+
+dados = {
+    "id": 1,
+    "nome": ["Juan", "Lemos"],
+    "idade": 19,
+    "detalhes": {
+        "hobbie": "surf",
+        "profissao": "J.A fiscal"
+    }
+}
+
+
+
